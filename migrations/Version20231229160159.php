e6d3746d2d2a9506ac5b0a5d251420d67af3c427
Ahamadi Sami <sami.ahamadi@laposte.net>
2024-01-01 18:42:36 +0100

    q25

--- migrations/Version20231229160159.php
+++ migrations/Version20231229160159.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20231229072742 extends AbstractMigration
+final class Version20231229160159 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -22,6 +22,7 @@ final class Version20231229072742 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('ALTER TABLE program ADD CONSTRAINT FK_92ED77847E3C61F9 FOREIGN KEY (owner_id) REFERENCES user (id)');
         $this->addSql('CREATE INDEX IDX_92ED77847E3C61F9 ON program (owner_id)');
+        $this->addSql('ALTER TABLE user ADD is_verified TINYINT(1) NOT NULL');
     }
 
     public function down(Schema $schema): void
@@ -29,5 +30,6 @@ final class Version20231229072742 extends AbstractMigration
         // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('ALTER TABLE program DROP FOREIGN KEY FK_92ED77847E3C61F9');
         $this->addSql('DROP INDEX IDX_92ED77847E3C61F9 ON program');
+        $this->addSql('ALTER TABLE user DROP is_verified');
     }
 }
